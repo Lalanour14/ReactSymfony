f26537da418a4032d1c1257b0cbe6242cef34ff6
Lalanour14 <bouzidselim@gmail.com>
2023-12-19 17:13:37 +0100

    ajout de dépandences stimulus

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,19 @@
+/*import { registerReactControllerComponents } from '@symfony/ux-react';*/
 
+/*import './bootstrap.js';*/
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+
+
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+
+registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
+registerReactControllerComponents(); 
 
 import'./styles/app.css';
+
+
---
+++ assets/react/controllers/Hello.jsx
@@ -0,0 +1,8 @@
+
+/*import React from 'react';
+
+export default function (props) {
+    console.log('test');
+    return <div>Hello {props.fullName}</div>;
+}*/
+console.log(test);
